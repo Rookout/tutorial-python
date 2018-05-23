6484a2e692eb5f08467f2042ed7ed5e41112933e
unknown <amit.shani1995@gmail.com>
2018-05-23 15:58:06 +0300

    added rook usage\ncreated bug with duplicate todo\ndockerized all things

---
+++ app.py
@@ -0,0 +1,98 @@
+from rook import auto_start
+import flask
+import re
+import os
+import string
+import random
+import json
+
+app = flask.Flask(__name__, static_url_path='/static')
+
+# no todos here just yet..
+todos = []
+
+# unsafeRandId generates a random string composed from english upper case letters and digits
+# it's called unsafe because it doesn't use a crypto random generator
+
+
+def unsafeRandId(len):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(len))
+
+
+def cleanStr(str):
+    return re.sub(r'[>|<|;|`|&|/|\\]', '', str)
+
+# redirect from base url to index.html
+
+
+@app.route("/")
+def index():
+    return flask.redirect('/static/index.html')
+
+
+@app.route('/todos/<todoId>', methods=['DELETE'])
+def del_todo(todoId):
+    global todos
+    newTodos = [t for t in todos if t['id'] != todoId]
+    todos = newTodos
+    return ('', 204)
+
+
+@app.route('/todos/clear_completed', methods=['DELETE'])
+def clear_completed():
+    #global todos
+    todos = [t for t in todos if not t['completed']]
+    return ('', 204)
+
+
+@app.route('/todos', methods=['UPDATE'])
+def update_todo():
+    global todos
+    req = flask.request.get_json()
+    for todo in todos:
+        if todo['id'] == req['id']:
+            todo['title'] = req['title']
+            todo['completed'] = req['completed']
+            break
+    return ('', 204)
+
+# add a new todo action
+
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    global todos
+    req = flask.request.get_json()
+    todoStr = cleanStr(req['title'])
+    if not todoStr:
+        return ('', 400)
+    todo = {
+        "title": cleanStr(req['title']),
+        "id": unsafeRandId(10),
+        "completed": False
+    }
+    todos.append(todo)
+    return ('', 204)
+
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    global todos
+    return json.dumps(todos)
+
+
+@app.route('/todo/dup/<todoId>', methods=['POST'])
+def duplicate_todo(todoId):
+    global todos
+    for todo in todos:
+        if todoId == todo['id']:
+            todos.append(
+                {'title': todo['title'],
+                 'id': unsafeRandId(10),
+                 'completed': todo['completed']})
+            break
+    return ('', 204)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
