15a3a4642d3c63965aa1cea76d88dcaec6513eba
Nir Yarden <42871571+niryarden@users.noreply.github.com>
2020-10-26 20:28:32 +0200

    RK-7456 - logtail // more logs (#35)
    * logtail // more logs
    
    * additions
    
    * forgot one log

--- utils/logging.py
+++ utils/logging.py
@@ -6,20 +6,29 @@ logger.setLevel(logging.INFO)
 
 
 def on_add_todo_logging(todoStr):
-    logger.info(flask.request.headers)
     logger.info("Successfully added a new task to do")
-    logger.info(f"The user needs to do - {todoStr}")
+    logger.debug(' ---- request http headers: ----')
+    for header in flask.request.headers:
+        logger.debug(header)
+    logger.debug(f"The user needs to do - @@@@@@@@ {todoStr} @@@@@@@@")
     logger.debug("add_todo() function works")
 
-    if not todoStr.isalnum():
+    if not todoStr.replace(" ", "").isalnum():
         logger.error("The user added a non-alphanumeric character to his task !!!")
         logger.debug("make sure your user is not a robot")
 
 
 def on_get_todos_logging(todos):
-    logger.info(flask.request.headers)
     logger.info("fetched the user's todo tasks")
+    logger.debug(' ---- request http headers: ----')
+    for header in flask.request.headers:
+        logger.debug(header)
     logger.debug("get_todos() function works")
+
+    logger.debug(' ---- all todos: ----')
+    for todo in todos:
+        status = "completed" if todo["completed"] else "uncompleted"
+        logger.debug(f"{todo['title']} --- {status}")
 
     if len(todos) == 0:
         logger.warning("The user have no tasks to do yet")
