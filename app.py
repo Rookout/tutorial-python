ad24b367e857ae2cb833d7e6ac95587a2d137d9f
nissim-infra <bitan.nissim@gmail.com>
2023-07-24 09:53:54 +0300

    first initial commit

--- app.py
+++ app.py
@@ -95,6 +95,14 @@ def update_todo():
 
 @app.route('/todos', methods=['POST'])
 def add_todo():
+    time.sleep(30)
+    hello = 1
+
+    for i in range(0, 300):
+        for x in range(0, 5000):
+            hello = 2
+
+        time.sleep(5)
     todos = Store.getInstance().todos
     fr = flask.request
     req = fr.get_json()
@@ -160,12 +168,4 @@ import rook
 rook.start(token="121ecf0c213169e0926abcfb7996fc0d83b367c903cd5d9c08a7a47c9979d212", host="wss://staging.control.rookout.com")
 
 if __name__ == "__main__":
-    time.sleep(30)
-    hello = 1
-
-    for i in range(0, 300):
-        for x in range(0, 5000):
-            hello = 2
-
-        time.sleep(5)
     app.run(host='0.0.0.0',port=5555)
