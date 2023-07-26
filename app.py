32f95fa5013ee6ab99e76d8339fb5ab848ebc862
nissim-infra <bitan.nissim@gmail.com>
2023-07-26 10:43:12 +0300

    first initial commit

--- app.py
+++ app.py
@@ -98,8 +98,8 @@ def add_todo():
     time.sleep(10)
     counter = 0
 
-    for i in range(0, 30):
-        for x in range(0, 10000):
+    for i in range(0, 120):
+        for x in range(0, 1000):
             counter = counter + 1
 
         print("sleeping, messages ", counter)
